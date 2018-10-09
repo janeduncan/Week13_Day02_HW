98ac0c6dda0bdeae44dd0c9fb63c726f54a723af
janeduncan <janeduncan@live.co.uk>
2018-10-10 00:03:40 +0100

    Adds User class

---
+++ src/main/java/com/example/codeclan/filesandfolders/models/User.java
@@ -0,0 +1,52 @@
+package com.example.codeclan.filesandfolders.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Folder> folders;
+
+    public User(String name) {
+        this.name = name;
+        this.folders = new ArrayList<Folder>();
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Folder> getFolders() {
+        return folders;
+    }
+
+    public void setFolders(List<Folder> folders) {
+        this.folders = folders;
+    }
+
+    public void addFolder(Folder folder) {
+        this.folders.add(folder);
+    }
+
+}
